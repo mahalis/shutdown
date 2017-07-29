8d7ab7fac434a79511463f182f5a47dbf191bc36
Noah Witherspoon <mahalis@gmail.com>
2017-07-28 21:02:44 -0700

    started on food stuff, cleaned up screen dimension handling

--- ld39.love/main.lua
+++ ld39.love/main.lua
@@ -5,14 +5,20 @@ local playerVelocity
 local currentFallRate
 local playerIsHoldingOn = true
 
-local BASE_FALL_RATE = 40
+local BASE_FALL_RATE = 80
 local FALL_RATE_ACCELERATION = 1
 local JUMP_SPEED = 600
-local DRAG = 1
+local PLAYER_DRAG = 1
+local MAX_FOOD_SPEED_VARIATION = 0.5 -- multiplier on current fall rate
+
+local foods = {}
+
+local screenWidth, screenHeight
 
 function love.load()
 	math.randomseed(os.time())
 
+	screenWidth, screenHeight = love.window.getMode()
 	setup()
 end
 
@@ -20,13 +26,15 @@ function setup()
 	currentFallRate = BASE_FALL_RATE
 	playerPosition = v(0,0)
 	playerVelocity = v(0,0)
+
+	foods = {}
 end
 
 function love.update(dt)
 	local totalVelocity = vMul(vAdd(playerVelocity, v(0, currentFallRate)), dt)
 	playerPosition = vAdd(playerPosition, totalVelocity)
 
-	local halfWidth = love.window.getMode() / 2
+	local halfWidth = screenWidth / 2
 	if playerPosition.x < -halfWidth then
 		playerPosition.x = -halfWidth + 1
 		playerVelocity.x = math.abs(playerVelocity.x)
@@ -35,16 +43,15 @@ function love.update(dt)
 		playerVelocity.x = -math.abs(playerVelocity.x)
 	end
 
-	playerVelocity = vMul(playerVelocity, 1 - DRAG * dt)
+	playerVelocity = vMul(playerVelocity, 1 - PLAYER_DRAG * dt)
 	currentFallRate = currentFallRate + FALL_RATE_ACCELERATION * dt
 end
 
 function love.draw()
-	local w, h = love.window.getMode()
 	local pixelScale = love.window.getPixelScale()
 	love.graphics.scale(pixelScale)
 
-	love.graphics.translate(w / 2, h / 2)
+	love.graphics.translate(screenWidth / 2, screenHeight / 2)
 	
 	love.graphics.setColor(255, 255, 255, 255)
 	love.graphics.circle("fill", playerPosition.x, playerPosition.y, 20)
@@ -67,6 +74,14 @@ end
 
 -- Utility stuff
 
+function makeFood()
+	local food = {}
+	food.speedAdjustment = frand()
+	food.x = frand() * screenWidth * 0.4
+	-- TODO: figure out how Y shit works — should spawn offscreen, obviously
+	return food
+end
+
 function drawCenteredImage(image, x, y, scale, angle)
 	local w, h = image:getWidth(), image:getHeight()
 	scale = scale or 1
@@ -75,12 +90,14 @@ function drawCenteredImage(image, x, y, scale, angle)
 end
 
 function mouseScreenPosition()
-	local w, h = love.window.getMode()
 	local pixelScale = love.window.getPixelScale()
 	local mouseX, mouseY = love.mouse.getPosition()
-	mouseX = (mouseX / pixelScale - w / 2)
-	mouseY = (mouseY / pixelScale - h / 2)
+	mouseX = (mouseX / pixelScale - screenWidth / 2)
+	mouseY = (mouseY / pixelScale - screenHeight / 2)
 	return v(mouseX, mouseY)
 end
 
+function frand()
+	return math.random() * 2 - 1
+end
 
